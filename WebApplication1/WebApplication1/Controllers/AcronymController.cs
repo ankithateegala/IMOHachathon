b729dd16f8336658590d5c44d1097cfbdf5e6d12
Junyu Wang <JWang@imo-online.com>
2016-09-23 18:36:11 -0500

    Add Controller

---
+++ WebApplication1/WebApplication1/Controllers/AcronymController.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using System.Web.Http;
+using Confluence.Model;
+
+namespace WebApplication1.Controllers
+{
+    [RoutePrefix("api/Acronyms")]
+    public class AcronymController : ApiController
+    {
+        [Route("")]
+        public IEnumerable<AcronymsInfo> GetAcronyms()
+        {
+            
+        }
+
+    }
+}

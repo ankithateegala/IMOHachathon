36a88cda6e92c3913b7f4766bb1439cca7885d1c
Junyu Wang <JWang@imo-online.com>
2016-09-24 08:59:39 -0500

    add

--- WebApplication1/IMOHackathon.App/app/main.js
+++ WebApplication1/IMOHackathon.App/app/main.js
@@ -1,13 +1,11 @@
 ﻿(function () {
     'use strict';
 
-    var myApp =  angular.module('app', []);
+    var myApp = angular.module('app', ['smart-table']);
     myApp.controller('MainController', main);
 
     function main() {
     }
-
-    ]);
 
     myApp.factory('whatsItFactory',
     ['$http', function ($http) {
@@ -20,40 +18,31 @@
 
     myApp.controller('whatsItController', ['$scope', 'whatsItFactory', function ($scope, whatsItFactory) {
         var url = 'http://imohackathon-1.xrzyyed2vm.us-west-2.elasticbeanstalk.com/api/Acronyms';
-        $scope.companies = [
-                    {
-                        'name': 'Infosys Technologies',
-                        'employees': 125000,
-                        'headoffice': 'Bangalore'
-                    },
-                        {
-                            'name': 'Cognizant Technologies',
-                            'employees': 100000,
-                            'headoffice': 'Bangalore'
-                        },
-                            {
-                                'name': 'Wipro',
-                                'employees': 115000,
-                                'headoffice': 'Bangalore'
-                            },
-                                {
-                                    'name': 'Tata Consultancy Services (TCS)',
-                                    'employees': 150000,
-                                    'headoffice': 'Bangalore'
-                                },
-                                    {
-                                        'name': 'HCL Technologies',
-                                        'employees': 90000,
-                                        'headoffice': 'Noida'
-                                    },
-        ];
         whatsItFactory.get(url).success(function (data) {
-            $scope.acronymns = data;
-        //}).error(function (error) {
-        //    // log errors
+            //$scope.acronymns = data;
+            $scope.rowCollection = data;
+            $scope.columnColletion = [
+                { label: 'Acronym', map: 'Acronym' },
+                { label: 'Full Name', map: 'Full_Name' },
+                { label: 'Description', map: 'Description' }
+            ];
+        }).error(function (error) {
+            // log errors
         });
     }])
 
 
+    //var thumbsUp = element(by.css('span.glyphicon-thumbs-up'));
+    //var thumbsDown = element(by.css('span.glyphicon-thumbs-down'));
+    //it('should check ng-show / ng-hide', function () {
+    //    expect(thumbsUp.isDisplayed()).toBeFalsy();
+    //    expect(thumbsDown.isDisplayed()).toBeTruthy();
+
+    //    element(by.model('whatsit')).click();
+    //    element(by.model('checked')).click();
+
+    //    expect(thumbsUp.isDisplayed()).toBeTruthy();
+    //    expect(thumbsDown.isDisplayed()).toBeFalsy();
+    //});
 
 })();

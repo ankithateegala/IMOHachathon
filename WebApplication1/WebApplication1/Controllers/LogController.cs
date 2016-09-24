3d906f8f7b6fa223a375e334d76366a15b21176b
Junyu Wang <JWang@imo-online.com>
2016-09-23 22:37:19 -0500

    Add log

---
+++ WebApplication1/WebApplication1/Controllers/LogController.cs
@@ -0,0 +1,10 @@
+﻿using System.Web.Http;
+using Confluence.Model;
+
+namespace WebApplication1.Controllers
+{
+    [RoutePrefix("Api/Log")]
+    public class LogController : ApiController
+    {
+    }
+}

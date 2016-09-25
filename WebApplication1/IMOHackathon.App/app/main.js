994d0d93a3c65906912eeb61da877dc9f516be02
Junyu Wang <JWang@imo-online.com>
2016-09-24 20:14:47 -0500

    Add

--- WebApplication1/IMOHackathon.App/app/main.js
+++ WebApplication1/IMOHackathon.App/app/main.js
@@ -2,24 +2,27 @@
     'use strict';
 
     var myApp = angular.module('app', []);
-    myApp.controller('MainController', main);
-
-    function main() {
-    }
+    myApp.controller('MainController',['$scope', function ($scope) {
+        $scope.showMe = false;
+        $scope.myFunc = function () {
+            $scope.showMe = !$scope.showMe;
+        }
+    }]);
 
     myApp.factory('whatsItFactory',
     ['$http', function ($http) {
         return{
-            get: function (url) {
+                get: function (url) {
                 return $http.get(url);
-            }
-        };
+        }
+    };
     }])
 
     myApp.controller('whatsItController', ['$scope', 'whatsItFactory', function ($scope, whatsItFactory) {
         var url = 'http://imohackathon-1.xrzyyed2vm.us-west-2.elasticbeanstalk.com/api/Acronyms';
         whatsItFactory.get(url).success(function (data) {
             $scope.rowCollection = data;
+
         }).error(function (error) {
             // log errors
         });
